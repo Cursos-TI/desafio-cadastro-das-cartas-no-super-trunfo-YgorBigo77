c7c68e18bef73f3e7fffdfd9a6159a9a6d1f10e5
YgorBigo77 <ygor.elias.45@gmail.com>
2025-08-13 21:49:15 +0000

    Criação da variavel Estado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,19 +4,34 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//Teste Ygor
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
+    char Estado;
+    char Carta;
+    char Cidade;
+    int populacao;
+    float Área;
+    float PIB;
+    int Pontos;
+
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    printf("Entre com o Estado: ");
+    scanf("%d", &Estado);
+
+    printf("Entre com o tamanho da polulação: ");
+    scanf("%d", &populacao);
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Estado: %d\n", Estado);
+    printf("A populacao tem: %d\n", populacao);
 
     return 0;
 }
